15429ba8c3e727715b637f6f5153b1ba07b42c33
Fernanda Manzano <manzanofernandadaniela@gmail.com>
2020-10-30 21:37:27 -0300

    clase titular

---
+++ Entities/Titular.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Titular
+    {
+        public char Adicional { get; set; }
+        public int Id { get; set; }
+        public char Apellido { get; set; }
+        public char Nombre { get; set; }
+        public int CUIL { get; set; }
+        public int DNI { get; set; }
+        public int Contacto { get; set; }
+
+        public int ContactoAlternativo { get; set; }
+        public char Correo { get; set; }
+    }
+}

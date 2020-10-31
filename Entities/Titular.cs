369ed4ecf3ee4f45485ed2ab6b4cb5764abc2235
Fernanda Manzano <manzanofernandadaniela@gmail.com>
2020-10-30 23:54:43 -0300

    clases

--- Entities/Titular.cs
+++ Entities/Titular.cs
@@ -9,14 +9,15 @@ namespace Entities
     public class Titular
     {
         public int Id { get; set; }
+
         public string Adicional { get; set; }
         
         public string Apellido { get; set; }
         public string Nombre { get; set; }
+
         public int CUIL { get; set; }
         public int DNI { get; set; }
         public int Contacto { get; set; }
-
         public int ContactoAlternativo { get; set; }
         public string Correo { get; set; }
     }

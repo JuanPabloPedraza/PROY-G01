9fb64f8feb38a39a0ca2177ba41450f188d586b0
Juan-pablo <51345057+JuanPedraza96@users.noreply.github.com>
2020-10-31 18:44:43 -0300

    AdminInterno Agregado

---
+++ AdminInterno/Admin.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace AdminInterno
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdmAltaUsuario = new System.Windows.Forms.Button();
+            this.cBAdmNO = new System.Windows.Forms.CheckBox();
+            this.cBAdmSi = new System.Windows.Forms.CheckBox();
+            this.txtAdmContraseña = new System.Windows.Forms.TextBox();
+            this.txtAdmUsuario = new System.Windows.Forms.TextBox();
+            this.txtAdmCorreoE = new System.Windows.Forms.TextBox();
+            this.txtAdmContacAltern = new System.Windows.Forms.TextBox();
+            this.txtAdmContacto = new System.Windows.Forms.TextBox();
+            this.txtAdmTitular = new System.Windows.Forms.TextBox();
+            this.txtAdmCuil = new System.Windows.Forms.TextBox();
+            this.txtAdmNombre = new System.Windows.Forms.TextBox();
+            this.txtAdmApellido = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.lblSolicitudes = new System.Windows.Forms.Label();
+            this.dGVAdmin = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVAdmin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdmAltaUsuario
+            // 
+            this.btnAdmAltaUsuario.Location = new System.Drawing.Point(590, 418);
+            this.btnAdmAltaUsuario.Name = "btnAdmAltaUsuario";
+            this.btnAdmAltaUsuario.Size = new System.Drawing.Size(125, 23);
+            this.btnAdmAltaUsuario.TabIndex = 78;
+            this.btnAdmAltaUsuario.Text = "Dar de alta Usuario";
+            this.btnAdmAltaUsuario.UseVisualStyleBackColor = true;
+            // 
+            // cBAdmNO
+            // 
+            this.cBAdmNO.AutoSize = true;
+            this.cBAdmNO.Location = new System.Drawing.Point(667, 287);
+            this.cBAdmNO.Name = "cBAdmNO";
+            this.cBAdmNO.Size = new System.Drawing.Size(42, 17);
+            this.cBAdmNO.TabIndex = 77;
+            this.cBAdmNO.Text = "NO";
+            this.cBAdmNO.UseVisualStyleBackColor = true;
+            // 
+            // cBAdmSi
+            // 
+            this.cBAdmSi.AutoSize = true;
+            this.cBAdmSi.Location = new System.Drawing.Point(609, 287);
+            this.cBAdmSi.Name = "cBAdmSi";
+            this.cBAdmSi.Size = new System.Drawing.Size(36, 17);
+            this.cBAdmSi.TabIndex = 76;
+            this.cBAdmSi.Text = "SI";
+            this.cBAdmSi.UseVisualStyleBackColor = true;
+            // 
+            // txtAdmContraseña
+            // 
+            this.txtAdmContraseña.Location = new System.Drawing.Point(615, 366);
+            this.txtAdmContraseña.Name = "txtAdmContraseña";
+            this.txtAdmContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtAdmContraseña.TabIndex = 75;
+            // 
+            // txtAdmUsuario
+            // 
+            this.txtAdmUsuario.Location = new System.Drawing.Point(615, 328);
+            this.txtAdmUsuario.Name = "txtAdmUsuario";
+            this.txtAdmUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtAdmUsuario.TabIndex = 74;
+            // 
+            // txtAdmCorreoE
+            // 
+            this.txtAdmCorreoE.Location = new System.Drawing.Point(609, 249);
+            this.txtAdmCorreoE.Name = "txtAdmCorreoE";
+            this.txtAdmCorreoE.Size = new System.Drawing.Size(100, 20);
+            this.txtAdmCorreoE.TabIndex = 73;
+            // 
+            // txtAdmContacAltern
+            // 
+            this.txtAdmContacAltern.Location = new System.Drawing.Point(609, 211);
+            this.txtAdmContacAltern.Name = "txtAdmContacAltern";
+            this.txtAdmContacAltern.Size = new System.Drawing.Size(100, 20);
+            this.txtAdmContacAltern.TabIndex = 72;
+            // 
+            // txtAdmContacto
+            // 
+            this.txtAdmContacto.Location = new System.Drawing.Point(609, 175);
+            this.txtAdmContacto.Name = "txtAdmContacto";
+            this.txtAdmContacto.Size = new System.Drawing.Size(100, 20);
+            this.txtAdmContacto.TabIndex = 71;
+            // 
+            // txtAdmTitular
+            // 
+            this.txtAdmTitular.Location = new System.Drawing.Point(609, 139);
+            this.txtAdmTitular.Name = "txtAdmTitular";
+            this.txtAdmTitular.Size = new System.Drawing.Size(100, 20);
+            this.txtAdmTitular.TabIndex = 70;
+            // 
+            // txtAdmCuil
+            // 
+            this.txtAdmCuil.Location = new System.Drawing.Point(609, 101);
+            this.txtAdmCuil.Name = "txtAdmCuil";
+            this.txtAdmCuil.Size = new System.Drawing.Size(100, 20);
+            this.txtAdmCuil.TabIndex = 69;
+            // 
+            // txtAdmNombre
+            // 
+            this.txtAdmNombre.Location = new System.Drawing.Point(609, 64);
+            this.txtAdmNombre.Name = "txtAdmNombre";
+            this.txtAdmNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtAdmNombre.TabIndex = 68;
+            // 
+            // txtAdmApellido
+            // 
+            this.txtAdmApellido.Location = new System.Drawing.Point(609, 26);
+            this.txtAdmApellido.Name = "txtAdmApellido";
+            this.txtAdmApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtAdmApellido.TabIndex = 67;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(539, 366);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 13);
+            this.label10.TabIndex = 66;
+            this.label10.Text = "Contraseña:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(551, 335);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 13);
+            this.label9.TabIndex = 65;
+            this.label9.Text = "Usuario:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(551, 287);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 13);
+            this.label8.TabIndex = 64;
+            this.label8.Text = "Titular:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(503, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 13);
+            this.label7.TabIndex = 63;
+            this.label7.Text = "Correo Electronico:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(491, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 13);
+            this.label6.TabIndex = 62;
+            this.label6.Text = "Contacto Alternativo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(537, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 61;
+            this.label5.Text = "Contacto:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(556, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 60;
+            this.label4.Text = "DNI:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(551, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "CUIL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(539, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 58;
+            this.label2.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(539, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 57;
+            this.label1.Text = "Apellido:";
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Location = new System.Drawing.Point(600, 3);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(109, 13);
+            this.lblDatos.TabIndex = 56;
+            this.lblDatos.Text = "Datos Usuario Nuevo";
+            // 
+            // lblSolicitudes
+            // 
+            this.lblSolicitudes.AutoSize = true;
+            this.lblSolicitudes.Location = new System.Drawing.Point(218, 8);
+            this.lblSolicitudes.Name = "lblSolicitudes";
+            this.lblSolicitudes.Size = new System.Drawing.Size(115, 13);
+            this.lblSolicitudes.TabIndex = 55;
+            this.lblSolicitudes.Text = "Solicitudes de Registro";
+            // 
+            // dGVAdmin
+            // 
+            this.dGVAdmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVAdmin.Location = new System.Drawing.Point(85, 48);
+            this.dGVAdmin.Name = "dGVAdmin";
+            this.dGVAdmin.Size = new System.Drawing.Size(400, 408);
+            this.dGVAdmin.TabIndex = 54;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 53;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 497);
+            this.Controls.Add(this.btnAdmAltaUsuario);
+            this.Controls.Add(this.cBAdmNO);
+            this.Controls.Add(this.cBAdmSi);
+            this.Controls.Add(this.txtAdmContraseña);
+            this.Controls.Add(this.txtAdmUsuario);
+            this.Controls.Add(this.txtAdmCorreoE);
+            this.Controls.Add(this.txtAdmContacAltern);
+            this.Controls.Add(this.txtAdmContacto);
+            this.Controls.Add(this.txtAdmTitular);
+            this.Controls.Add(this.txtAdmCuil);
+            this.Controls.Add(this.txtAdmNombre);
+            this.Controls.Add(this.txtAdmApellido);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDatos);
+            this.Controls.Add(this.lblSolicitudes);
+            this.Controls.Add(this.dGVAdmin);
+            this.Controls.Add(this.button1);
+            this.Name = "Admin";
+            this.Text = "Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.dGVAdmin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdmAltaUsuario;
+        private System.Windows.Forms.CheckBox cBAdmNO;
+        private System.Windows.Forms.CheckBox cBAdmSi;
+        private System.Windows.Forms.TextBox txtAdmContraseña;
+        private System.Windows.Forms.TextBox txtAdmUsuario;
+        private System.Windows.Forms.TextBox txtAdmCorreoE;
+        private System.Windows.Forms.TextBox txtAdmContacAltern;
+        private System.Windows.Forms.TextBox txtAdmContacto;
+        private System.Windows.Forms.TextBox txtAdmTitular;
+        private System.Windows.Forms.TextBox txtAdmCuil;
+        private System.Windows.Forms.TextBox txtAdmNombre;
+        private System.Windows.Forms.TextBox txtAdmApellido;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDatos;
+        private System.Windows.Forms.Label lblSolicitudes;
+        private System.Windows.Forms.DataGridView dGVAdmin;
+        private System.Windows.Forms.Button button1;
+    }
+}
